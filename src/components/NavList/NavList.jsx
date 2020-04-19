95a6b4f76ea3bc78b676d4dfaa5080741ead8d59
AstRioh <korova674@gmail.com>
2020-04-19 17:07:26 +0800

    small refactoring

--- src/components/NavList/NavList.jsx
+++ src/components/NavList/NavList.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import "./NavList.scss";
 import Dot from '../Dot/Dot';
 
-const NavList = ({ tasks, deletable, onClick }) => {
+const NavList = ({ items, deletable, onClick }) => {
     return (
     <ul onClick={onClick} className="nav-list">
         {
-            tasks.map(task => (
-                <li key={task.id} className={task.active ? 'nav-list__item nav-list__item_active' : 'nav-list__item'}>
-                    {task.icon ? <i><img src={task.icon} alt={task.text + ' icon'} className="nav-list__item-image" /></i> : <i><Dot color={task.color}/></i>}
-                    <span className="nav-list__item-text">{task.text}</span>
+            items.map(item => (
+                <li key={item.id} className={item.active ? 'nav-list__item nav-list__item_active' : 'nav-list__item'}>
+                    {item.icon ? <i><img src={item.icon} alt={item.text + ' icon'} className="nav-list__item-image" /></i> : <i><Dot color={item.color}/></i>}
+                    <span className="nav-list__item-text">{item.text}</span>
                 </li>
             ))
         }
