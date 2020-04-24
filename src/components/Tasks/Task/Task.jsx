600f01ff77e28b4f9f42cac98aa14a164cb6b88c
AstRioh <korova674@gmail.com>
2020-04-25 03:13:53 +0800

    layout for edit and remove task buttons

--- src/components/Tasks/Task/Task.jsx
+++ src/components/Tasks/Task/Task.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import editImg from '../../../assets/img/edit.png';
 import './Task.scss';
 
 const Task = ({ text, id, completed }) => {
@@ -7,7 +8,11 @@ const Task = ({ text, id, completed }) => {
         <div className="task">
             <input id={`do-${text}-${id}`} type="checkbox" className="task__check" readOnly />
             <label htmlFor={`do-${text}-${id}`} className="task__custom-check"></label>
-            <input type="textarea" value={text} className="task__text" />
+            <p className="task__text">{text}</p>
+            <div className="task__controls">
+                <img src={editImg} className="task__edit" alt="Edit task"/>
+                <div className="task__delete"></div>
+            </div>
         </div>
     );
 }
